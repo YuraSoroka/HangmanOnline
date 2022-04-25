b358bd071327baefef93bbbc19e37e5a6a6d1d19
Yurii Soroka <yurasoroka1999@gmail.com>
2022-04-25 15:06:52 +0300

    Project configuration

---
+++ HangmanOnline/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HangmanOnline.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
